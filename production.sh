a47078f53cad71ac53afd35c36fb22b096766b40
Duc <ducbuiquangxd@gmail.com>
2018-01-19 14:41:39 +0700

    :recycle: Update production script [ci skip]

--- production.sh
+++ production.sh
@@ -1,23 +1,22 @@
 if [ -z "$1" ]
-  then
-    echo "No argument supplied. Use ./production.sh {version}. For example: ./production.sh 1.1.0"
-    exit
+	  then
+	    echo "No argument supplied. Use ./production.sh {version}. For example: ./production.sh 1.1.0"
+	    exit
 fi
-
 # Checkout to master branch
 git checkout master
 # Pull the latest code from master branch
-git pull https://github.com/buiquangduc/WP_Default_Feature_Image.git master
-# Checkout to production branch
-git checkout -b production
+git pull git@github.com:buiquangduc/WP_Default_Feature_Image.git master
+# Create new branch
+git checkout -b $1
 # Running build script
 bash build.sh
 # Remove development stuff
-git rm -rf --cached assets build.sh composer.json composer.lock node_modules package-lock.json package.json storage webpack.config.js yarn.lock
+rm .gitignore
+rm -rf assets build.sh composer.json composer.lock node_modules package-lock.json package.json storage webpack.config.js yarn.lock yarn-error.log
 # Commit and push to production branch
 git add .
 git commit -m ":tada: Production version $1 $(date)"
-git push https://github.com/buiquangduc/WP_Default_Feature_Image.git production
+git push git@github.com:buiquangduc/WP_Default_Feature_Image.git $1
 # Checkout to master branch
 git checkout master
-
